292fb54f99369762c48ddfd32956bdc6f021d727
Godning <736397792@qq.com>
2017-07-21 07:28:03 +0000

    update main.js

--- main/main.js
+++ main/main.js
@@ -1,3 +1,17 @@
 module.exports = function main(email, suffixes) {
   // Write your code here
+  var list = email.split('@');
+  suf = list[1];
+  var flag = 0;
+  for(let s of suffixes){
+      if(s == suf){
+          flag = 1;
+      }
+  }
+  if(flag == 1){
+      return true;
+  }
+  else{
+      return false;
+  }
 };
